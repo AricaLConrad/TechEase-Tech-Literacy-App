eb00caf144170e76cf93a328fe97458bc0a21087
NatalmanNahm <natalnahm@gmail.com>
2021-03-17 20:31:17 -0500

    Add the help screen.

--- app/src/main/java/com/example/techease/MainActivity.java
+++ app/src/main/java/com/example/techease/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExtendedFloatingActionButton helpFab = findViewById(R.id.help_button);
+        helpFab.setOnClickListener(v -> {
+            Intent helpIntent = new Intent(this, HelpActivity.class);
+            startActivity(helpIntent);
+        });
     }
 
 /*    public void goToMediaActivity(View view) {
